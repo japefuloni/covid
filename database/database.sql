4d5aa8b1957e3b7ac3b21773e85b76bc4da6b677
jmantillap <jmantillap@gmail.com>
2020-06-03 15:42:31 -0500

    prueba

--- database/database.sql
+++ database/database.sql
@@ -18,7 +18,7 @@ USE `covidform`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `administradores`
+-- Table structure for table `administradores` ok
 --
 
 DROP TABLE IF EXISTS `administradores`;
