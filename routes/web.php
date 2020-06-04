dfb3f3a7da15381b899b103cc4400debc2b48e6d
jmantillap <jmantillap@gmail.com>
2020-06-04 17:10:19 -0500

    Se agrega funcionalidad de autenticacion

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+include ('web-admin.php');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 
 Route::get('/', function () {
     return view('welcome');
